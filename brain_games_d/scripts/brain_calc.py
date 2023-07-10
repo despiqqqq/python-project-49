46cce6f35e70fc0761f447a99a521d8d9f04f111
despiqqqq <117080938+despiqqqq@users.noreply.github.com>
2023-07-10 19:52:46 +0300

    fix module

--- brain_games_d/scripts/brain_calc.py
+++ brain_games_d/scripts/brain_calc.py
@@ -1,11 +1,10 @@
 import prompt
 import random
+from brain_games import main as intro_main
 
 
 def main():
-    print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    name = intro_main()
     print('What is the result of the expression?')
     correct_answers = 0
 
@@ -52,6 +51,7 @@ def main():
                 return
 
     print(f"Congratulations, {name}!")
+
 
 if __name__ == '__main__':
     main()
--- brain_games_d/scripts/brain_games.py
+++ brain_games_d/scripts/brain_games.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+import prompt
+
 from brain_games_d.cli import welcome_user
 
 
 def main():
     print("Welcome to the Brain Games!")
-    welcome_user()
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return name
 
 
 if __name__ == '__main__':
     main()
-
