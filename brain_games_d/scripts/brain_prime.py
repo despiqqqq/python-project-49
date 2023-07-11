d01169ad48b259c9db0196ab721e94dcfc08f580
despiqqqq <117080938+despiqqqq@users.noreply.github.com>
2023-07-11 20:27:39 +0300

    Update brain_prime.py

--- brain_games_d/scripts/brain_prime.py
+++ brain_games_d/scripts/brain_prime.py
@@ -1,3 +1,4 @@
+python
 import random
 import prompt
 from brain_games import main as intro_main
@@ -12,23 +13,38 @@ def is_prime(n):
     return True
 
 
-def main():
+def greet_user():
     name = intro_main()
+    return name
+
+
+def ask_question():
+    number = random.randint(1, 100)
+    print("Question:", number)
+    answer = input("Your answer: ")
+    return (number, answer)
+
+
+def check_answer(number, answer):
+    if (answer == "yes" and is_prime(number)) or (answer == "no" and not is_prime(number)):
+        print("Correct!")
+        return True
+    else:
+        correct_answer = "yes" if is_prime(number) else "no"
+        print("'" + answer + "' is wrong answer ;(. Correct answer was '" + correct_answer + "'.")
+        return False
+
+
+def main():
+    name = greet_user()
     print("Answer 'yes' if given number is prime. Otherwise answer 'no'.")
     correct_answers = 0
 
     while correct_answers < 3:
-        number = random.randint(1, 100)
-
-        print("Question:", number)
-        answer = input("Your answer: ")
-
-        if (answer == "yes" and is_prime(number)) or (answer == "no" and not is_prime(number)):
-            print("Correct!")
+        number, answer = ask_question()
+        if check_answer(number, answer):
             correct_answers += 1
         else:
-            correct_answer = "yes" if is_prime(number) else "no"
-            print("'" + answer + "' is wrong answer ;(. Correct answer was '" + correct_answer + "'.")
             print("Let's try again, " + name + "!")
             break
 
