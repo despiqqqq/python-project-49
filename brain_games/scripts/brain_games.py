c112df3885e38978e29980fe1f5621410b6889f0
despiqqqq <117080938+despiqqqq@users.noreply.github.com>
2023-07-13 21:56:59 +0300

    вынесение логики welcome_user

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 import prompt
+from brain_games.scripts.cli import welcome_user as welcome_user
 
 
 def main():
-    print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    return name
+    welcome_user()
 
 
 if __name__ == '__main__':
