00b5568b80463866c859a4dea870c183d72a1fc7
despiqqqq <117080938+despiqqqq@users.noreply.github.com>
2023-07-13 06:21:51 +0300

    Update brain_calc.py

--- brain_games_d/scripts/brain_calc.py
+++ brain_games_d/scripts/brain_calc.py
@@ -3,52 +3,45 @@ import random
 from brain_games import main as intro_main
 
 
+def generate_question():
+    oper = random.randint(1, 3)  # определение случайной операции
+
+    # Генерация первого случайного числа
+    random_number1 = random.randint(1, 10)
+
+    # Генерация второго случайного числа
+    random_number2 = random.randint(1, 10)
+
+    if oper == 1:
+        question = f'Question: {random_number1} + {random_number2}'
+        correct_answer = str(random_number1 + random_number2)
+    elif oper == 2:
+        question = f'Question: {random_number1} - {random_number2}'
+        correct_answer = str(random_number1 - random_number2)
+    else:
+        question = f'Question: {random_number1} * {random_number2}'
+        correct_answer = str(random_number1 * random_number2)
+
+    return question, correct_answer
+
+
 def main():
     name = intro_main()
     print('What is the result of the expression?')
     correct_answers = 0
 
     while correct_answers < 3:
-        oper = random.randint(1, 3)  # определение случайной операции
+        question, correct_answer = generate_question()
+        print(question)
+        user_answer = prompt.string('Your answer: ')
 
-        # Генерация первого случайного числа
-        random_number1 = random.randint(1, 10)
-
-        # Генерация второго случайного числа
-        random_number2 = random.randint(1, 10)
-
-        if oper == 1:
-            print(f'Question: {random_number1} + {random_number2}')
-            res = prompt.string('Your answer: ')
-            if random_number1 + random_number2 == int(res):
-                print('Correct!')
-                correct_answers += 1
-            else:
-                print(f"'{res}' is wrong answer ;(. Correct answer was '{random_number1 + random_number2}'.")
-                print(f"Let's try again, {name}!")
-                return
-
-        if oper == 2:
-            print(f'Question: {random_number1} - {random_number2}')
-            res = prompt.string('Your answer: ')
-            if random_number1 - random_number2 == int(res):
-                print('Correct!')
-                correct_answers += 1
-            else:
-                print(f"'{res}' is wrong answer ;(. Correct answer was '{random_number1 - random_number2}'.")
-                print(f"Let's try again, {name}!")
-                return
-
-        if oper == 3:
-            print(f'Question: {random_number1} * {random_number2}')
-            res = prompt.string('Your answer: ')
-            if random_number1 * random_number2 == int(res):
-                print('Correct!')
-                correct_answers += 1
-            else:
-                print(f"'{res}' is wrong answer ;(. Correct answer was '{random_number1 * random_number2}'.")
-                print(f"Let's try again, {name}!")
-                return
+        if user_answer == correct_answer:
+            print('Correct!')
+            correct_answers += 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
 
     print(f"Congratulations, {name}!")
 
