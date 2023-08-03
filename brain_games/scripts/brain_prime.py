92ce1618052d35ee226c348a6a9c04347254aa0d
despiqqqq <117080938+despiqqqq@users.noreply.github.com>
2023-08-03 19:23:21 +0300

    fix prime

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -2,11 +2,11 @@
 
 
 from brain_games.brain_game_l import play_game
-from brain_games.games import brain_progression_l
+from brain_games.games import brain_prime_l
 
 
 def main():
-    play_game(brain_progression_l)
+    play_game(brain_prime_l)
 
 
 if __name__ == '__main__':
